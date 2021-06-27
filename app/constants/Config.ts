883e585c10f982cfdfc1946dfac9d12d088483db
HamzaKV <hamzakv333@gmail.com>
2021-06-26 23:44:34 -0400

    Update Config.ts

--- app/constants/Config.ts
+++ app/constants/Config.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({
-    UNSPLASH_ACCESS_KEY: 'COFWAL5OcDn4T0LnvRjv96e4--0MSzeBCoQ9_gXJt-8',
+    UNSPLASH_ACCESS_KEY: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY,
     UNSPASH_URL: 'https://api.unsplash.com'
 });
